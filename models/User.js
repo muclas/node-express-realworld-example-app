3ba77474407a44805b6eb7c99fa00fa3fa866745
muclas <mucsi.laszlo77@gmail.com>
2017-05-28 09:05:12 +0200

    Creating profiles

--- models/User.js
+++ models/User.js
@@ -43,4 +43,13 @@ UserSchema.methods.toAuthJSON = function() {
     };
 };
 
+UserSchema.methods.toProfileJSONFor = function() {
+    return {
+        username: this.username,
+        bio: this.bio,
+        image: this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
+        following: false
+    };
+};
+
 mongoose.model('User', UserSchema);
