f74042b91e08f5b6c4c4c3713549bd6217900e8e
muclas <mucsi.laszlo77@gmail.com>
2017-05-28 09:09:16 +0200

    adding favoriting functionality

--- models/User.js
+++ models/User.js
@@ -10,7 +10,8 @@ var UserSchema = new mongoose.Schema({
     bio: String,
     image: String,
     hash: String,
-    salt: String
+    salt: String,
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
 }, { timestamp: true });
 
 UserSchema.methods.setPassword = function(password) {
@@ -43,13 +44,32 @@ UserSchema.methods.toAuthJSON = function() {
     };
 };
 
-UserSchema.methods.toProfileJSONFor = function() {
+UserSchema.methods.toProfileJSONFor = function(user) {
     return {
         username: this.username,
         bio: this.bio,
         image: this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
         following: false
     };
+};
+
+UserSchema.methods.favorite = function(id) {
+    if (this.favorites.indexOf(id) === -1) {
+        this.favorites.push(id);
+    }
+    
+    return this.save();
+};
+
+UserSchema.methods.unfavorite = function(id) {
+    this.favorites.remove(id);
+    return this.save();
+};
+
+UserSchema.methods.isFavorite = function(id) {
+    return this.favorites.some(function(favoriteId) {
+        return favoriteId.toString() === id.toString();
+    });
 };
 
 mongoose.model('User', UserSchema);
